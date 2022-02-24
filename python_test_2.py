bfd50ab5a10528edf5174d12fc525f582caf0286
FelixHarris <felhar75@kingshigh.school.nz>
2022-02-25 12:24:34 +1300

    data_types.py

--- python_test_2.py
+++ python_test_2.py
@@ -1,6 +1,24 @@
+"""
 print("Felix Harrison")
 print(84*24)
 print(15)
+
 print("King's High School")
 print("2047")
 print("115")
+print(658/7)
+print(2022-1969)
+print("Witcher 3")
+print("DTE")
+
+print(5+10.0) #add a float to an integer
+print(10-5.0) #subtract a float from an integer
+print(5/2)
+print(5//2)
+print(5%2)
+
+print("hello world" * 5)
+"""
+str(55+65)
+int(55.0)
+float(44+10)
