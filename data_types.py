347a1ffd803f9352ee8b9e0906d1a8245941d566
FelixHarris <felhar75@kingshigh.school.nz>
2022-02-28 13:23:45 +1300

    Update data_types.py

---
+++ data_types.py
@@ -0,0 +1,17 @@
+"""
+print("There's a zombie on your lawn\nThere's a zombie on your lawn\nThere's a zombie on your lawn\nWe don't want zombies on the lawn.")#1
+print("Give me rent.")#2
+print("Strings are a collection of characters.")#3
+print("An integer is a whole number that can be used with floats.")#4
+print("A float is a decimal number and can be used with integers.")#5
+print(543/3)#6
+print(543//3)#7
+print(543%3)#8
+"""
+print(1000%9)#9
+print(434//10)#10
+print(3%2)
+print(5%2)
+print(555/2)
+print(123/2)
+print(123//2)
