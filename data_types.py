cc429809b7ddfdb4ad6a191694669c194655c846
FelixHarris <felhar75@kingshigh.school.nz>
2022-03-01 14:57:40 +1300

    data_types.py

--- data_types.py
+++ data_types.py
@@ -7,11 +7,30 @@ print("A float is a decimal number and can be used with integers.")#5
 print(543/3)#6
 print(543//3)#7
 print(543%3)#8
-"""
+
 print(1000%9)#9
 print(434//10)#10
-print(3%2)
-print(5%2)
-print(555/2)
-print(123/2)
-print(123//2)
+print(3%2)#11
+print(5%2)#12
+print(555/2)#13
+print(123/2)#14
+print(123//2)#15
+print(123%2)#16
+print(764%2)#17
+
+print(165//2)#18
+print(2.01+2.05)#19
+print(2+4)#20
+print(4-3.21)#21
+print(3.21-2)#22
+print(5/5)#23
+print(1.1*5)#24
+print(5*5)#25
+print("Felix"*10)#26
+
+print("Felix\n"*10)#27
+print("5+10=" + str(5+10))#28
+"""
+print("15/2=" + str(15/2))#29
+print("30/2*5=" + str(30/2*5))#30
+print("30/2*5=" + str(30//2*5))
