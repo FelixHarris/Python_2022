9e63e8f9af7640dcb19057959c4d41c4b20d38e3
FelixHarris <felhar75@kingshigh.school.nz>
2022-03-03 09:55:45 +1300

    variable practice.py

---
+++ variable_practice.py
@@ -0,0 +1,25 @@
+player_lives=21#1
+player_username="riley"#2
+player_money=1#3
+
+print("\n")
+phone_number = input("What is your phone number?")#4
+user_name= input("What is your name?")#5
+user_age= input("How old are you?")#6
+year=input("what is the year?")#7
+print(f"your name is {user_name}.")#8
+print(f"you are {user_age}.")#9
+print(f"The year is {year}.")#10
+
+print("\n")
+surname= input("what is your surname?")#11
+user_school= input("What is the name of your school?")#12
+user_favourite_food=input("What is your favourite food?")#13
+user_pet_name=input("What is your pet's name?")#14
+
+print("\n")
+user_chocolate=5#15
+user_score=2#16
+user_high_score=3#17
+laps=2%3#18
+player_ammo=12%30
