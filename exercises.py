a18b466da0bf58a32295157d022e704ee0e6a853
FelixHarris <felhar75@kingshigh.school.nz>
2022-03-07 12:25:00 +1300

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -1,3 +1,4 @@
+"""
 input("Press any key and then press enter.")#1
 input("Type your name and press enter.")#2
 input("Type your age and press enter.")#3
@@ -5,5 +6,17 @@ input("Type your age and press enter.")#3
 name=input("What is your name?")#4
 age=input("How old are you?")#5
 favourite_movie=("What is your favourite movie?")#6
-book=input("Name a book.")#6
-
+book=input("Name a book.")#7
+user_adjective=("Name an adjctive")#8
+user_noun=("Name a noun")#9
+user_verb=("Name a verb")#10
+print(f"Hello {name}.")#11
+print(f"You are {age} years old.")
+print(f"{favourite_movie} is bad.")
+print(f"{book} is good.")
+print(f"{user_adjective} is a good describing word im going to use that more.")
+print(f"{user_noun} is stupid.")
+print(f"{user_verb} is dumb.")
+"""
+user_age=input("How old are you?")#12
+int(f"{user_age}"+10)
