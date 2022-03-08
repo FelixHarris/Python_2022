ae925281446e8c2144f2d714d3f6488895ce95a9
FelixHarris <felhar75@kingshigh.school.nz>
2022-03-08 13:25:04 +1300

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -17,6 +17,29 @@ print(f"{book} is good.")
 print(f"{user_adjective} is a good describing word im going to use that more.")
 print(f"{user_noun} is stupid.")
 print(f"{user_verb} is dumb.")
+
+user_age=int(input("How old are you?"))#12
+print(f" you will be {user_age + 10} in 10 years")#13
+print(f" you were born in {2022 - user_age}")#14
+user_apples=int(input("How many apples do you have?"))
+print(f"You have {user_apples} apples.")#15
+friends_check=int(input("How many friends does Riley have"))
+print(f"{friends_check}")#16
+
+print(f"{user_apples/friends_check} you can have this many apples each.")#17
+
+user_pizza=int(input("How many pizzas do you want?"))#18
+user_feed=int(input("How many people do you want to feed?"))#19
+print(f"Each person gets{user_pizza*8/user_feed}")#20
+user_dollars=int(input("How many dollars do you have?"))#21
+user_tv_price=int(input("How much does a tv cost?"))#22
+print(f"You will have {user_dollars-user_tv_price} if you bought the tv.")#23
+print(f"The tv will cost {0.8*user_tv_price} if it was 20% off.")#24
+
+user_bitcoin=float(input("How many bitcoins do you have."))#25
+bitcoin_cost=(55,661.97)#26
+print(f"you have ${bitcoin_cost*user_bitcoin} bitcoin heres a gold medal.")#27
 """
-user_age=input("How old are you?")#12
-int(f"{user_age}"+10)
+user_wage=int(input("How much do you earn per week?"))#28
+user_tax=float(input("How much is the tax as a decimal?"))#29
+print(f"{user_wage-user_wage*taxed_money} ")
