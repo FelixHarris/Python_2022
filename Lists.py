b2eb063a6920cc8be6d84491455df2ddedb6485c
FelixHarris <felhar75@kingshigh.school.nz>
2022-03-14 10:56:16 +1300

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,4 +1,15 @@
-fibonacci=[0,1,1,2,3,5,8,13,21,34]
+fibonacci=[0,1,1,2,3,5,8,13,21,34]#1
 print(fibonacci)
-fruits=["bananas,bananas,bananas,bananas,bananas"]
-print(fruits)
+fruits=["bananas","bananas","bananas","bananas","bananas"]#2
+print(f"{','.join(fruits)}")
+youtubers=["Annoying Orange","Fred", "Markiplier","EDP445","Call me Carson","Deji","XQC","Riley","Nich Eh 30","Chandler","Fps Russia","Twomad"]
+print(f"{','.join(youtubers)}")
+music=[]
+music.append("Gummy Bear song")
+music.append("Diggy Hole")
+music.append("Popsico")
+music.append("The Mask")
+music.append("Friday")
+music.append("What does the sock say")
+music.append("Fortnite Rap")
+music.append()
