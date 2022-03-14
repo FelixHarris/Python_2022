4536650b2c31c0597c449b414592d552ee744597
FelixHarris <felhar75@kingshigh.school.nz>
2022-03-15 12:25:21 +1300

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,3 +1,4 @@
+"""
 fibonacci=[0,1,1,2,3,5,8,13,21,34]#1
 print(fibonacci)
 fruits=["bananas","bananas","bananas","bananas","bananas"]#2
@@ -5,11 +6,43 @@ print(f"{','.join(fruits)}")
 youtubers=["Annoying Orange","Fred", "Markiplier","EDP445","Call me Carson","Deji","XQC","Riley","Nich Eh 30","Chandler","Fps Russia","Twomad"]
 print(f"{','.join(youtubers)}")
 music=[]
-music.append("Gummy Bear song")
+music.append("Gummy Bear song")#4
 music.append("Diggy Hole")
 music.append("Popsico")
 music.append("The Mask")
 music.append("Friday")
 music.append("What does the sock say")
 music.append("Fortnite Rap")
-music.append()
+print(','.join(music))
+
+books=[]#5
+books.append("Karl incident")
+books.append("Witcher 1")
+books.append("Witcher 2")
+books.append("Witcher 3")
+books.append("Witcher 4")
+print(','.join(books))
+"""
+pizza_toppings=[]
+pizza_toppings1=input("What pizza toppings do you want?")
+if pizza_toppings1 != "":
+	pizza_toppings.append(pizza_toppings1)
+pizza_toppings1=input("What pizza toppings do you want?")
+if pizza_toppings1 != "":
+	pizza_toppings.append(pizza_toppings1)
+pizza_toppings1=input("What pizza toppings do you want?")
+if pizza_toppings1 != "":
+	pizza_toppings.append(pizza_toppings1)
+pizza_toppings1=input("What pizza toppings do you want?")
+if pizza_toppings1 != "":
+	pizza_toppings.append(pizza_toppings1)
+pizza_toppings1=input("What pizza toppings do you want?")
+if pizza_toppings1 != "":
+	pizza_toppings.append(pizza_toppings1)
+pizza_toppings1=input("What pizza toppings do you want?")
+if pizza_toppings1 != "":
+	pizza_toppings.append(pizza_toppings1)
+print(pizza_toppings)
+
+
+fruits["banana","deez nutz","Riley","Apple","Orange"]
