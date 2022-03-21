ed8bdc6f600ba0f3214d4900596489e77f27a482
FelixHarris <felhar75@kingshigh.school.nz>
2022-03-22 09:59:36 +1300

    Update new_test.py

--- new_test.py
+++ new_test.py
@@ -18,11 +18,57 @@ def comb_hair():
 		in the red basket.
 		""")
 
-space = room("""
+space = Room("""
 	You are drifting in space. It feels very cold.
 	A slate_blue spaceship sits completely silently to your left,
 	its airlock open and waiting.
 	""")
+
+spaceship=Room("""
+	The bridge if the spaceship is shiny and white, with thousands of
+	 small, red, blinking lights.
+	""")
+
+quarters=Room("""
+	There are Beds.""")
+
+cargo=Room("""
+	Vent, SUS.""")
+
+hallway=Room("""
+	Red is actting kinda SUS.""")
+
+mess_hall=Room("""
+	Messi and Ronaldo.""")
+
+docking=Room("""
+	Riley.""")
+bridge=Room("""
+	Etika""")
+escape_pods(""")
+
+
+current_room=spaceship
+
+
+@when("enter airlock")
+@when("enter spaceship")
+@when("enter ship")
+def enter_spaceship():
+	global  current_room
+	if current_room is not space:
+		say("There is no airlock here")
+		return
+	else:
+		current_room=spaceship
+		print("""You heave yourself into the spaceship and
+			slam you hand on the button to close the door.
+			""")
+		print(current_room)
+
+
+
+
 
 
 def main():
