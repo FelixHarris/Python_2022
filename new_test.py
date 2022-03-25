1de14baf74775b4dae0e200ef04e37fea0499c07
FelixHarris <felhar75@kingshigh.school.nz>
2022-03-25 13:25:56 +1300

    Update new_test.py

--- new_test.py
+++ new_test.py
@@ -2,22 +2,7 @@ from adventurelib import*
 
 Room.items=Bag()
 
-
-
-
-@when("brush teeth")
-def brush_teeth():
-	print("you brush your teeth")
-
-@when("comb hair")
-@when("comb")
-def comb_hair():
-	say("""
-		You brush your long flowing locks with
-		the gold hairbrush that you have selected from the
-		in the red basket.
-		""")
-
+#ROOMS
 space = Room("""
 	You are drifting in space. It feels very cold.
 	A slate_blue spaceship sits completely silently to your left,
@@ -67,7 +52,7 @@ def enter_spaceship():
 			""")
 		print(current_room)
 
-
+#Connections
 spaceship.east=hallway
 spaceship.south=quarters
 hallway.east=bridge
@@ -77,14 +62,15 @@ cargo.south=hallway
 quarters.east=mess_hall
 bridge.south=escape_pods
 
+
+#Items
 Item.description=""#this adds a blank description to each item
 
-mess_hall.items.add(red_keycard)
-cargo.items.add(knife)
+
 knife=Item("a dirty knife","Knife")
 knife.description="the knife has a dull sheen to it but it looks rather sharp."
 
-red_keycard= Item("A red keycard","Keycard","red card") 
+red_keycard= Item("A red keycard","Keycard","red card","red keycard") 
 red_keycard.description="Its a red keycard. It probably opens a door or a locker."
 
 red_button=Item("Red button","Button")
@@ -92,25 +78,71 @@ red_button.description=("Vote out the imposter")
 
 
 bat=Item("bat")
-red_button.description=("its a bat")
+bat.description=("its a bat")
+
+#Bags
+mess_hall.items.add(red_keycard)
+cargo.items.add(knife)
+cargo.items.add(bat)
+quarters.items.add(red_button)
+
+
+#Add items into bag
+@when("get ITEM")
+@when("take ITEM")
+@when("pick up ITEM")
+def pickup(item):
+	if item in current_room.items:
+		t=current_room.items.take(item)
+		inventory.add(t)
+		print(f"You pick up the {item}")
+	else:
+		print(f"you dont see a {item}")
 
 
 
-
+#Binds
 @when ("go DIRECTION")
 def travel(direction):
 	global current_room
 	if direction in current_room.exits():
 		current_room=current_room.exit(direction)
-		print(f"You go{direction}.")
+		print(f"You go {direction}.")
 		print(current_room)
+		print(current_room.exits())
+	else:
+		print("You cant go that way.")
 
 @when("look")
 def look():
 	print(current_room)
 	print(f"there are exits to the{current_room.exits()}.")
-	if len(current_room.titles) > 0:
-		print("You ")
+	if len(current_room.items) > 0:
+		print("You also see:")
+		for item in current_room.items:
+			print(item)
+
+
+
+#Functions
+@when("inventory")
+@when("show inventory")
+@when("what is in my pocket")
+def player_inventory():
+	print("You are carrying")
+	for item in inventory:
+		print(item)
+
+
+@when("look at ITEM")
+def look_at(item):
+	if item in inventory:
+		t = inventory.find(item)
+		print(t.description)
+	else:
+		print(f"You arent carrying an {item}")
+
+
 
 
 
