81acf651e2d9b42245f88698aff715b0a222beae
FelixHarris <100237106+FelixHarris@users.noreply.github.com>
2022-03-31 21:13:40 +1300

    Update Abandoned_Lab.py

--- Abandoned_Lab.py
+++ Abandoned_Lab.py
@@ -1,5 +1,7 @@
 from adventurelib import*
 
+Room.items=Bag()
+
 
 #Room Descriptions
 entrance = Room(""" It's a white room with doors in all 4 directions. 
@@ -26,8 +28,76 @@ open_lab = Room (""" As you walk in your nose achs in pain after smelling the ch
 	There are doors to the North, West and East.""")
 
 
-Supply_Room = Room(""" The room is stacked full of boxes, chemicals, and other useful equipment. 
-	One thing catches your eye, a hazmat suit.""")
+supply_Room = Room(""" The room is stacked full of boxes, chemicals, and other useful equipment. 
+	Something catches your eye, that thing being a hazmat suit.""")
+
+computation_dry_lab = Room(""" You walk into the room, it seems to be a normal lab. 
+	There are lab coats hanging from a wall, one lab coat has has something in its pocket. """)
+
+specialized_wet_lab=Room()
+
+security_room=Room()
+
+cloning_room=Room()
+
+
+#Changing Rooms
+current_room=entrance
+inventory=Bag()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
